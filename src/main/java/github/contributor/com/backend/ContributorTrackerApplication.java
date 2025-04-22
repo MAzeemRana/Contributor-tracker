adaf2756b21980d70d9c1ff6b15835771c35beac
Muhammad Azeem Sikandar Rana <m.azeemas82005@gmail.com>
2025-04-22 14:28:46 +0500

    Frontend

---
+++ src/main/java/github/contributor/com/backend/ContributorTrackerApplication.java
@@ -0,0 +1,13 @@
+package github.contributor.com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContributorTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContributorTrackerApplication.class, args);
+	}
+
+}
